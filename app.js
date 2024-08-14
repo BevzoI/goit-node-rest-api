70bcd29d6df52e9c0e2a873015d95ea0032d601e
BevzoI <bevzo.i.n@gmail.com>
2024-08-14 22:24:02 +0200

    upp app

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
-import dotenv from 'dotenv';
-dotenv.config();
+import "dotenv/config";
 
 import sequelize from "./db/sequelize.js";
 
