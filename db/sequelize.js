e4b52898862aeecf7e5b91aaad754a6091508f08
BevzoI <bevzo.i.n@gmail.com>
2024-08-14 22:21:52 +0200

    add env

--- db/sequelize.js
+++ db/sequelize.js
@@ -1,15 +1,24 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize } from "sequelize";
+
+const {
+  DATABASE_DIALECT,
+  DATABASE_NAME,
+  DATABASE_HOST,
+  DATABASE_PORT,
+  DATABASE_USER,
+  DATABASE_PASSWORD,
+} = process.env;
 
 const sequelize = new Sequelize({
-    dialect: 'postgres',
-    host: 'dpg-cqrqcgo8fa8c73d8p2cg-a.frankfurt-postgres.render.com',
-    username: 'iryna',
-    password: 'hu9eqjB3jbbftWkxYQ9R57VYTpAhitwz',
-    database: 'db_contacts_q3eq',
-    port: '5432',
-    dialectOptions: {
-        ssl: true,
-    }
+  dialect: DATABASE_DIALECT,
+  database: DATABASE_NAME,
+  host: DATABASE_HOST,
+  port: DATABASE_PORT,
+  username: DATABASE_USER,
+  password: DATABASE_PASSWORD,
+  dialectOptions: {
+    ssl: true,
+  },
 });
 
 export default sequelize;
