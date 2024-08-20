00dace8fbd586ca106a140a716fc5499ba5c43a1
BevzoI <bevzo.i.n@gmail.com>
2024-08-20 18:40:59 +0200

    upp router

--- db/models/Contact.js
+++ db/models/Contact.js
@@ -39,8 +39,3 @@ User.hasMany(Contact, { foreignKey: 'owner' });
 Contact.belongsTo(User, { foreignKey: 'owner' });
 
 export default Contact;
-
-
-// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiaWF0IjoxNzI0MTU1OTg3LCJleHAiOjE3MjQ1ODc5ODd9.g2W9k1NG0ryj1iqPqMEAIHPnZqMIDw8cfuUnGw6zVy4
-// Authorization
-// Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiaWF0IjoxNzI0MTU1OTg3LCJleHAiOjE3MjQ1ODc5ODd9.g2W9k1NG0ryj1iqPqMEAIHPnZqMIDw8cfuUnGw6zVy4
