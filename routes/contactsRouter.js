04c5ea7085c6163d98f5df509b906dd0a0caa712
BevzoI <bevzo.i.n@gmail.com>
2024-08-20 12:56:57 +0200

    add file

---
+++ routes/contactsRouter.js
@@ -0,0 +1,28 @@
+import express from "express";
+import {
+  getAllContacts,
+  getOneContact,
+  deleteContact,
+  createContact,
+  updateContact,
+  updateContactFavorite
+} from "../controllers/contactsControllers.js";
+import { authenticate } from "../middlewares/authenticate.js";
+
+const contactsRouter = express.Router();
+
+contactsRouter.use(authenticate); 
+
+contactsRouter.get("/", getAllContacts);
+
+contactsRouter.get("/:id", getOneContact);
+
+contactsRouter.delete("/:id", deleteContact);
+
+contactsRouter.post("/", createContact);
+
+contactsRouter.put("/:id", updateContact);
+
+contactsRouter.patch("/:id/favorite", updateContactFavorite);
+
+export default contactsRouter;
